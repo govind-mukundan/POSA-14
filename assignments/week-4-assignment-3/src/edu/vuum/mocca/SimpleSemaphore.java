cde2e722767c48a64189a3cc2a7fec35721aea7e
Govind Mukundan <govind.mukundan@gmail.com>
2014-06-15 22:57:44 +0800

    Completed Ping-pong assignment synch with semaphores and barrier latch

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,36 +1,45 @@
 package edu.vuum.mocca;
-import java.util.concurrent.locks.Condition;
+
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
 
 /**
  * @class SimpleSemaphore
  * 
  * @brief This class provides a simple counting semaphore implementation using
- *        Java a ReentrantLock and a ConditionObject (which is accessed via a
- *        Condition). It must implement both "Fair" and "NonFair" semaphore
- *        semantics, just liked Java Semaphores.
+ *        Java a ReentrantLock and a ConditionObject. It must implement both
+ *        "Fair" and "NonFair" semaphore semantics, just liked Java Semaphores.
  */
 public class SimpleSemaphore {
     /**
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
-
+	private final ReentrantLock lock;
     /**
-     * Define a Condition that waits while the number of permits is 0.
+     * Define a ConditionObject to wait while the number of
+     * permits is 0.
      */
     // TODO - you fill in here
-
+	private final Condition waitWhileEmpty; 
     /**
      * Define a count of the number of available permits.
      */
-    // TODO - you fill in here. Make sure that this data member will
+    // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+	private volatile int count;
 
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+    	lock = new ReentrantLock(fair);
+    	waitWhileEmpty  = lock.newCondition();
+    	if(permits > 0) // Not allowed to have a -ve count
+    		count = permits;
+    	else
+    		count = 0;
     }
 
     /**
@@ -39,6 +48,14 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+        lock.lockInterruptibly();
+        try {
+          while (count == 0)
+        	  waitWhileEmpty.await(); // We're going to wait till we get a permit
+          --count;
+        } finally {
+          lock.unlock();
+        }
     }
 
     /**
@@ -47,6 +64,14 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+        lock.lock();
+        try {
+          while (count == 0)
+        	  waitWhileEmpty.awaitUninterruptibly();
+          --count;
+        } finally {
+          lock.unlock();
+        }
     }
 
     /**
@@ -54,13 +79,30 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+        lock.lock();
+        try {
+        ++count;
+        waitWhileEmpty.signal();
+        }
+        finally{
+        lock.unlock();
+        }
     }
 
     /**
      * Return the number of permits available.
      */
     public int availablePermits() {
-        // TODO - you fill in here to return the correct result
-    	return 0;
+        // TODO - you fill in here by changing null to the appropriate
+        // return value.
+        lock.lock();
+        try {
+        int ret = count;
+        return ret;
+        }
+        finally{
+        lock.unlock();
+        }
+        
     }
 }
